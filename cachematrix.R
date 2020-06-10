f941844cd0778bae21478fa0648eb49f08fa4822
tompkind <tompkins.daniel@gmail.com>
2020-06-10 13:27:03 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,20 @@
 makemat <- function(x = matrix()) {
-        
+     ## defines argument "x" as matrix   
 	s <- NULL
-        
+			## define s and initialize as NULL
 	set <- function(y) {
-                
+            ## define function "set"    
 		x <<- y
-                
+                 ## y is assigned to x in x's parent environment
 		s <<- NULL
-        
+              ## NULL is assigned to s in its parent environment
 	}
         
 
 	get <- function() x
-        
+        			## define function "get", returns "x"
 	setsolve <- function(solve) s <<- solve
-        
+         ## define function "setsolve", assigns inverse of matrix to "s"
 	getsolve <- function() s
         
 	list(set = set, get = get,
@@ -29,7 +29,8 @@ makemat <- function(x = matrix()) {
 cachesolve <- function(x = matrix(), ...) {
 
 	s <- x$getsolve()
-        
+        			## if s is not null anywhere in the global environment
+   							## then it returns the value of s
 	if(!is.null(s)) {
 
         	message("getting cached data")
@@ -40,11 +41,11 @@ cachesolve <- function(x = matrix(), ...) {
         
 
 	data <- x$get()
-
+					## if s is not defined anywhere in the global environment
         s <- solve(data, ...)
-
+				## then it assigns x to data, and then inverse of data to
         x$setsolve(s)
-        
+        				## s and returns that value.
 	s
 
 }
